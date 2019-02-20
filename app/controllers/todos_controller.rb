4f78e50eed4885f6787d75b9aea3e7a169a33da5
josereyessaldias <jpreyess@uc.cl>
2019-02-20 20:38:59 -0300

    ya cree el index, new y create :)

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -8,6 +8,20 @@ class TodosController < ApplicationController
 		@todo = Todo.new
 	end
 
+	def create
+		@todo = Todo.new(todo_params)
+		@todo.save
+		redirect_to todos_path
+	end
 
+
+
+
+
+	private
+
+	def todo_params
+		params.require(:todo).permit(:description, :completed)
+	end
 
 end
