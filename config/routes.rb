a5a082bf82f249c4f74108f0483bb883a0fbcab5
josereyessaldias <jpreyess@uc.cl>
2019-02-20 21:39:47 -0300

    ya agregué edit y update

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 	get 'todos/new'
 	post 'todos', to: 'todos#create'
 	get 'todos/:id', to: 'todos#show', as: 'todo'
+	get 'todos/:id/edit', to: 'todos#edit', as: 'edit_todo'
+	patch 'todos/:id', to: 'todos#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
